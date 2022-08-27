178302e830389c74acec8a10658e8043f27aa35e
Roalvies <roalvies@gmail.com>
2022-08-27 15:09:43 -0400

    Reestructuracion

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -28,15 +28,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/*
- *Añadimos otro comentario
- *pa ver si con esta wea sirv Puta la wea ctme
- *
- *Puta la wea
- *
- *
- * /
-
 /**
  * @author sotobotero This set of test allow check the behavior of the web layer
  * application listening like as it would do in production, sending and http
@@ -91,11 +82,6 @@ public class BasicApplicationTests {
                 .content(asJsonString(mockdto))
         ).andDo(print()).andExpect(status().isOk());
     }
-
-    /*
-     *Esto es una prueba para webhook
-     *para la wea ctm
-     */
 
     /**
      * Test call of create method, on weblayer.
